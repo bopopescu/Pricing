365f2f6f23b51f0807fa80f09f6f9d9d2f9b79d9
Oscar Macias <oscarefren_1000@hotmail.com>
2020-06-26 17:30:14 -0500

    fix

--- backend/custom_azure.py
+++
@@ -1,7 +0,0 @@
-from storages.backends.azure_storage import AzureStorage
-
-class AzureMediaStorage(AzureStorage):
-    account_name = 'lgklataforma' # Must be replaced by your <storage_account_name>
-    account_key = 'SpHagQjk7C4dBPv1cse9w36zmAtweXIMjcw9DWve7ipgXgf2Fa5l+vw2k57EM8uinlUOkfxt34BQpC9FBHE+Yg==' # Must be replaced by your <storage_account_key>
-    azure_container = 'archivospricing'
-    expiration_secs = None
--- usersadmon/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsersadmonConfig(AppConfig):
-    name = 'usersadmon'
